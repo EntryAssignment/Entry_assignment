6100cf75160d7014bf85b0c8a8ceb32d053ac6d2
in-jun <injuninjune@gmail.com>
2024-04-06 22:20:51 +0900

    init project

---
+++ src/test/java/org/example/entryassignment/EntryAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.entryassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
