86ce975dc97106d0f7fdf11133bedb8f84dd680d
Hannah <sunday0206@dsm.hs.kr>
2024-04-11 19:46:02 +0900

    수정

--- src/main/java/org/example/entryassignment/user/dto/response/UserResponse.java
+++ src/main/java/org/example/entryassignment/user/dto/response/UserResponse.java
@@ -1,13 +1,11 @@
 package org.example.entryassignment.user.dto.response;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.example.entryassignment.user.entity.Member;
 
 @Setter
 @RequiredArgsConstructor
-public class ResponseUserDTO {
+public class UserResponse {
     private String userId;
     private String userEmail;
     private String userPw;
--- src/main/java/org/example/entryassignment/user/service/ViewService.java
+++ src/main/java/org/example/entryassignment/user/service/ViewService.java
@@ -2,18 +2,29 @@ package org.example.entryassignment.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.entryassignment.user.dto.request.UserRequest;
+import org.example.entryassignment.user.dto.response.UserResponse;
 import org.example.entryassignment.user.entity.Member;
 import org.example.entryassignment.user.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ViewService {
-    private final MemberRepository memberRepository;
+    private MemberRepository memberRepository;
+    private UserResponse response;
 
-    public void View(UserRequest request) {
-        Member member = new Member(request);
-        memberRepository.findById(member);
+    public long memberView(long id){
+        Optional<Member> memberOptional = memberRepository.findById(id);
+        if (memberOptional.isPresent()){
+            return response.toMemberDTO(memberOptional.get()); // optional을 벗겨내서 entity -> dto 변환
+        }else {
+            return null;
+        }
+
+
     }
 
 }
