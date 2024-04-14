c15fc015ab9f5571ed6d72b5e1c3e068a0ee0e66
Hannah <sunday0206@dsm.hs.kr>
2024-04-14 22:35:57 +0900

    마지막

--- src/main/java/org/example/entryassignment/user/controller/MemberController.java
+++ src/main/java/org/example/entryassignment/user/controller/MemberController.java
@@ -2,20 +2,35 @@ package org.example.entryassignment.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.entryassignment.user.dto.request.UserRequest;
+import org.example.entryassignment.user.entity.Member;
+import org.example.entryassignment.user.service.DeleteIdService;
 import org.example.entryassignment.user.service.SignupService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.entryassignment.user.service.ViewService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final SignupService signupService;
+    private final DeleteIdService deleteIdService;
+    private final ViewService viewService;
+
     @PostMapping("/signup")
     public void signup(@RequestBody UserRequest userRequest) {
         signupService.signup(userRequest);
+    }
+
+    @GetMapping("/{id}")
+    public UserRequest memberView(@PathVariable("id") ) {
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void DeleteId (@PathVariable("id") Member request) {
+        Member member = new Member(request);
+        deleteIdService.DeleteId(member);
+
     }
 
 }
